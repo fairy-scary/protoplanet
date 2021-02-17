a6f41f16adf5db11363a3af88a4317c574f21041
fairy-scary <ecoautumn@gmail.com>
2021-02-17 14:10:17 -0600

    bug fix

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,17 +1,17 @@
-// const config = require('./index');
+const config = require('./index');
 
-// const db = config.db;
-// const username = db.username;
-// const password = db.password;
-// const database = db.database;
-// const host = db.host;
+const db = config.db;
+const username = db.username;
+const password = db.password;
+const database = db.database;
+const host = db.host;
 
 module.exports = {
   development: {
-    "username": "protoplanet",
-    "password": "proto1",
-    "database": "protoplanet",
-    "host": "127.0.0.1",
+    "username": username,
+    "password": password,
+    "database": database,
+    "host": host,
     "dialect": "postgres",
     "seedStorage": "sequelize"
   },
